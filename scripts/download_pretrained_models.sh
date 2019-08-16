ef08723fae82409a65dd72ad46d66b6fbb60b023
xzhou33 <xiongzho@amazon.com>
2019-08-16 14:34:09 -0700

    fix pretrained models download link

--- scripts/download_pretrained_models.sh
+++ scripts/download_pretrained_models.sh
@@ -4,9 +4,8 @@ if [ ! -d "pretrained" ]; then
     mkdir "pretrained"
 fi
 
-# Download VGG16
-wget https://s3-us-west-2.amazonaws.com/domain-adaptation-exps/models/pretrained/vgg16.params -O pretrained/vgg16.params
-wget https://s3-us-west-2.amazonaws.com/domain-adaptation-exps/models/pretrained/resnet101_v2.params -O pretrained/resnet101_v2.params
-wget https://s3-us-west-2.amazonaws.com/domain-adaptation-exps/models/pretrained/resnet152_v2.params -O pretrained/resnet152_v2.params
-wget https://s3-us-west-2.amazonaws.com/domain-adaptation-exps/models/pretrained/densenet169.params -O pretrained/densenet169.params
-wget https://s3-us-west-2.amazonaws.com/domain-adaptation-exps/models/pretrained/densenet201.params -O pretrained/densenet201.params
+wget https://domain-adaptation-exps.s3-us-west-2.amazonaws.com/models/pretrained/vgg16.params -O pretrained/vgg16.params
+wget https://domain-adaptation-exps.s3-us-west-2.amazonaws.com/models/pretrained/resnet101_v2.params -O pretrained/resnet101_v2.params
+wget https://domain-adaptation-exps.s3-us-west-2.amazonaws.com/models -O /pretrained/resnet152_v2.params 
+wget https://domain-adaptation-exps.s3-us-west-2.amazonaws.com/models/pretrained/densenet169.params -O pretrained/densenet169.params
+wget https://domain-adaptation-exps.s3-us-west-2.amazonaws.com/models/pretrained/densenet201.params -O pretrained/densenet201.params 
